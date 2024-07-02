a7f638d6efb7b7df3c8a5b3705e65bad4f184093
ImanolP01 <imanolpizana006@icloud.com>
2024-07-02 09:32:12 -0400

    list in dictionaries

--- Reflex.py
+++ Reflex.py
@@ -1,15 +1,16 @@
 #### Reflex Game
 import os
 
-attempts = {
-    "bob" : {
+attempts = [
+     {
+        "Name": "bob",
         "Graduation": "1988",
         "Time": 3
     }
-}
+]
 
 previousUser = "bob"
-counter = 0
+counter = 1
 repeat = True
 while(repeat):
 
@@ -17,28 +18,26 @@ while(repeat):
     graduation = input("What year did you graduate? ")
     time = int(input("#: "))
 
-    user = { name : { "Graduation": graduation, "Time": time }}
+    user = { "Name": name, "Graduation": graduation, "Time": time }
 
-    attempts.update(user)
+    attempts.append(user)
 
-    previousTime = attempts[previousUser]["Time"]
-    if counter > 10:
-        if time < previousTime:
-            del attempts[previousUser]
-            previousUser = name
-            previousTime = time
-        elif time > previousTime:
-            del attempts[name]
-
+    
     counter += 1
     print(counter)
 
-    for x, user in attempts.items():
-        print(x)
-        for time in user.items():
-            print(time)
+    var = attempts[0]['Time'] 
+    print(var)
 
+    if var > time:
+        attempts.pop(0)
+    
     print(attempts)
+
+    ### sorts dictionaries in list based on Time 
+    attempts = sorted(attempts, key=lambda x: x['Time'])
+      
+
 
     try_again = input("Press anything but 0 to try again. ")
     if try_again == "0":
